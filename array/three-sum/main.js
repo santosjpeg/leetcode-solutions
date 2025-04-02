7692f3f925c5a0866c197d18460d59920fe21be6
aaron <santosaaron2004@gmail.com>
2025-04-02 15:15:42 -0400

    3sum

--- array/three-sum/main.js
+++ array/three-sum/main.js
@@ -3,18 +3,29 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-  let sol = [];
+  nums = nums.sort((a, b) => a - b);
+  let solution = [];
 
-  let i = 0, j = i + 1; k = j + 1;
-  let pivot = nums[i];
-  for (j; j < nums.length - 1; j++) {
-    for (k; k < nums.length; k++) {
-      if (pivot + nums[j] + nums[k] === 0)
-        sol.push([nums[i], nums[j], nums[k]]);
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] === nums[i - 1])
+      continue;
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+      if (sum < 0)
+        left++;
+      else if (sum > 0)
+        right--;
+      else {
+        solution.push([nums[i], nums[left++], nums[right--]]);
+        while (nums[left] === nums[left - 1])
+          left++;
+        while (nums[right] === nums[right + 1])
+          right--;
+      }
     }
   }
 
-  return sol;
-};
-
-console.log(threeSum([-1, 0, 1, 2, -1, -4]));
+  return solution;
+}
