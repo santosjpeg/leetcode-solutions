17e60274f77b1d438b198306318fb92cd07f5ae1
aaron <santosaaron2004@gmail.com>
2025-04-03 09:57:35 -0400

    update longest substr:

--- strings/longest-substring-without-repeats/main.js
+++ strings/longest-substring-without-repeats/main.js
@@ -6,14 +6,20 @@ var lengthOfLongestSubstring = function (s) {
   let set = new Set();
   let left = 0;
   let right = 0;
-
   let max = 0;
-  while (right < s.length) {
-    while (set.has(s[right])) {
 
+  //attempt to expand window by iterating right ptr
+  while (right < s.length) {
+    //if right contains duplicates; iterate by removing left
+    //until condition is met
+    while (set.has(s[right])) {
       set.delete(s[left++]);
     }
+
+    //append right and expand
     set.add(s[right++]);
+
+    //before the end of outer loop, check if length of susbtr > max
     if (right - left > max)
       max = right - left;
   }
